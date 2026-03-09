6263b1af4a4c3459015cd8c2350c4f0ad5322e0c
Guilherme Faglioni <98924709+GuilhermeFaglioni@users.noreply.github.com>
2026-03-09 16:19:16 -0300

    feat: clean arch folders #2

---
+++ atomik-api/src/main/java/com/atomik/atomik_api/AtomikApiApplication.java
@@ -0,0 +1,13 @@
+package com.atomik.atomik_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtomikApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtomikApiApplication.class, args);
+	}
+
+}
---
+++ atomik-api/src/test/java/com/atomik/atomik_api/AtomikApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atomik.atomik_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtomikApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
